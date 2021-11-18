2033ed209975925faaab99ac254a58de12b6e088
Raimund Canzler <git@rai-canzler.de>
2021-11-18 16:45:54 +0100

    feat: add jpeg route

--- src/controllers/digit.controller.ts
+++ src/controllers/digit.controller.ts
@@ -1,51 +1,62 @@
 import Jimp from "jimp";
+import { Blob } from "buffer";
 import { Route, Tags, Get, Post, Path, Body, Query } from "tsoa";
 import { Digit } from "../models/digit";
 import * as Repository from "../repositories/digit";
 
 export interface IDigitRequestPayload {
-	value: number;
-	image: string;
+    value: number;
+    image: string;
 }
 
 @Route("digits")
 @Tags("Digit")
 export default class DigitController {
-	@Get("/")
+    @Get("/")
     public async getDigits(@Query() fields?: string[]): Promise<Array<Digit>> {
         return Repository.getDigits(fields);
-	}
+    }
 
-	@Post("/")
-	public async createDigit(
-		@Body() body: IDigitRequestPayload
-	): Promise<Digit> {
-		const imageWithoutPrefix = body.image.replace(
-			"data:image/jpeg;base64,",
-			""
-		);
+    @Post("/")
+    public async createDigit(
+        @Body() body: IDigitRequestPayload
+    ): Promise<Digit> {
+        const imageWithoutPrefix = body.image.replace(
+            "data:image/jpeg;base64,",
+            ""
+        );
 
-		const imageBuffer = Buffer.from(imageWithoutPrefix, "base64");
+        const imageBuffer = Buffer.from(imageWithoutPrefix, "base64");
 
-		const image = await Jimp.read(imageBuffer).then(
-			async (img) =>
-				await img
-					.resize(64, 64)
-					.quality(80)
-					.getBufferAsync(Jimp.MIME_JPEG)
-		);
+        const image = await Jimp.read(imageBuffer).then(
+            async (img) =>
+                await img
+                    .resize(64, 64)
+                    .quality(80)
+                    .getBufferAsync(Jimp.MIME_JPEG)
+        );
 
         return Repository.createDigit({ ...body, image });
-	}
+    }
 
-	@Get("/count")
-	public async countDigits(): Promise<number> {
+    @Get("/count")
+    public async countDigits(): Promise<number> {
         return Repository.countDigits();
     }
-	}
 
-	@Get("/:id")
-	public async getDigit(@Path() id: string): Promise<Digit | null> {
+    @Get("/:id/jpeg")
+    public async getDigitAsJpeg(@Path() id: string): Promise<Buffer> {
+        const digit = await Repository.getDigit(Number(id));
+
+        if (!digit) {
+            throw Error("No digit with this id");
+        }
+
+        return digit.image;
+    }
+
+    @Get("/:id")
+    public async getDigit(@Path() id: string): Promise<Digit | null> {
         return Repository.getDigit(Number(id));
-	}
+    }
 }
--- src/routes/digit.router.ts
+++ src/routes/digit.router.ts
@@ -4,7 +4,7 @@ import DigitController from "../controllers/digit.controller";
 const router = Router();
 
 interface DigitStoreRequest extends Request {
-	body: { value: number; image: string };
+    body: { value: number; image: string };
 }
 
 interface DigitGetAllRequest extends Request {
@@ -12,34 +12,41 @@ interface DigitGetAllRequest extends Request {
 }
 
 router.get("/", async (req: DigitGetAllRequest, res: Response) => {
-	const controller = new DigitController();
+    const controller = new DigitController();
     const response = await controller.getDigits(req.query.fields?.split(","));
 
-	res.send(response);
+    res.send(response);
 });
 
 router.post("/", async (req: DigitStoreRequest, res: Response) => {
-	console.log("Received digit:", req.body.value);
+    console.log("Received digit:", req.body.value);
 
-	const controller = new DigitController();
-	const response = await controller.createDigit(req.body);
+    const controller = new DigitController();
+    const response = await controller.createDigit(req.body);
 
-	return res.send(response);
+    return res.send(response);
 });
 
 router.get("/count", async (_, res: Response) => {
-	const controller = new DigitController();
-	const response = await controller.countDigits();
+    const controller = new DigitController();
+    const response = await controller.countDigits();
 
-	res.send(response);
+    res.send(response);
+});
+
+router.get("/:id/jpeg", async (req: Request, res: Response) => {
+    const controller = new DigitController();
+    const response = await controller.getDigitAsJpeg(req.params.id);
+
+    res.type("image/jpeg").send(response);
 });
 
 router.get("/:id", async (req: Request, res: Response) => {
-	const controller = new DigitController();
-	const response = await controller.getDigit(req.params.id);
+    const controller = new DigitController();
+    const response = await controller.getDigit(req.params.id);
 
-	if (!response) return res.status(404).send({ message: "No digit found" });
-	return res.send(response);
+    if (!response) return res.status(404).send({ message: "No digit found" });
+    return res.send(response);
 });
 
 export default router;
